2bb7419008304452bfb97c506487c1ba8ef63e97
kofigo375 <kofigo375@gmail.com>
2022-07-06 22:12:44 +0000

    added more methods to method.py

--- methods.py
+++ methods.py
@@ -15,31 +15,53 @@ class customer:
         print(self.age)
         print(self.gender)
 
+# an example of a static method
+# static methods are applied on the class itself
+    def output_sometext():
+        print("welcome customer")
 
- #creating a customer
-customer_1 = customer("Ephraim", "22", "male")
-#print(customer_1.age, customer_1.gender)
+#the string method allows us to print our customer object as a string
+    def __str__(self):
+        return self.name + " " + self.age + " " + self.gender
+# a method to display all customers objects
+    def print_all_customers(customers):
+        for customer in customers:
+            print(customer)
 
-#customer_2 = customer("Mensah", "14", "female")
-#print(customer_2.name, customer_2.age)
+# the equal method 
+# to check if two customers are equal
+    def __eq__(self, other):
+        if self.name == other.name and self.age == other.age:
+            return True
+        else:
+            return False
+
+
 
 #creating a list of customers
 customers = [
-    customer("Isaac", "76", "male"),
+    customer("Isaac", "670", "male"),
     customer("musah", "90", "woman"),
     customer("Newton","24","male"),
     customer("keren","28","female")
 ]
 #print(customers[2].gender)
 
-
-
-
-
+# updating a customer age with my update_age method
 customers[2].update_age("670") 
 print(customers[2].age) 
-#customer_1.update_age("99")
-#print(customer_1.age)
-#customers[1].update_age("45")
-#  print(customers[1].update_age)
+
+#printing customer at index position 1 using my display method
 customers[1].display()
+
+#printing "some text" using a static method
+customer.output_sometext()
+
+#printing some customer object as a string
+print(customers[2])
+
+# printing all the customers using my print_all_costomers method
+customer.print_all_customers(customers)
+
+#checking if two customers are equal
+print(customers[0] == customers[2])
